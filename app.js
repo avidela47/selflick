108120b58901480d7352f58699112e5acee01734
Ariel Videla <ariel_videla@hotmail.com>
2024-07-18 09:52:37 -0300

    chatbot

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 let menuVisible = false;
 //Función que oculta o muestra el menu
-function mostrarOcultarMenu(){
-    if(menuVisible){
-        document.getElementById("nav").classList ="";
+function mostrarOcultarMenu() {
+    if (menuVisible) {
+        document.getElementById("nav").classList = "";
         menuVisible = false;
-    }else{
-        document.getElementById("nav").classList ="responsive";
+    } else {
+        document.getElementById("nav").classList = "responsive";
         menuVisible = true;
     }
 }
-function seleccionar(){
+function seleccionar() {
     //oculto el menu una vez que selecciono una opcion
     document.getElementById("nav").classList = "";
     menuVisible = false;
@@ -24,17 +24,17 @@ let thumbnails = document.querySelectorAll('.thumbnail .item');
 let countItem = items.length;
 let itemActive = 0;
 // event next click
-next.onclick = function(){
+next.onclick = function () {
     itemActive = itemActive + 1;
-    if(itemActive >= countItem){
+    if (itemActive >= countItem) {
         itemActive = 0;
     }
     showSlider();
 }
 //event prev click
-prev.onclick = function(){
+prev.onclick = function () {
     itemActive = itemActive - 1;
-    if(itemActive < 0){
+    if (itemActive < 0) {
         itemActive = countItem - 1;
     }
     showSlider();
@@ -43,7 +43,7 @@ prev.onclick = function(){
 let refreshInterval = setInterval(() => {
     next.click();
 }, 5000)
-function showSlider(){
+function showSlider() {
     // remove item active old
     let itemActiveOld = document.querySelector('.slider .list .item.active');
     let thumbnailActiveOld = document.querySelector('.thumbnail .item.active');
@@ -77,19 +77,45 @@ document.addEventListener('DOMContentLoaded', () => {
     const zoomClose = document.getElementsByClassName('zoom-close')[0];
 
     zoomables.forEach(img => {
-        img.addEventListener('click', function() {
+        img.addEventListener('click', function () {
             zoomModal.style.display = 'block';
             zoomedImg.src = this.src;
         });
     });
 
-    zoomClose.addEventListener('click', function() {
+    zoomClose.addEventListener('click', function () {
         zoomModal.style.display = 'none';
     });
 
-    zoomModal.addEventListener('click', function(event) {
+    zoomModal.addEventListener('click', function (event) {
         if (event.target === zoomModal) {
             zoomModal.style.display = 'none';
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const contactoSection = document.getElementById('contacto');
+    const chatbot = document.getElementById('chatbot');
+
+    window.addEventListener('scroll', () => {
+        const sectionPosition = contactoSection.getBoundingClientRect();
+        const isVisible = sectionPosition.top < window.innerHeight && sectionPosition.bottom >= 0;
+
+        if (isVisible) {
+            anime({
+                targets: '#chatbot',
+                bottom: '20px',
+                easing: 'easeOutExpo',
+                duration: 1000,
+            });
+        } else {
+            anime({
+                targets: '#chatbot',
+                bottom: '-100%',
+                easing: 'easeOutExpo',
+                duration: 1000,
+            });
         }
     });
 });
